cbf279e8f282954200db10cc4e2721e85d05a0d5
alpineq <alpineq@protonmail.com>
2026-03-01 18:47:14 +0300

    FIX: added en_GB to stop bambu studio from crashing

--- configuration.nix
+++ configuration.nix
@@ -90,6 +90,10 @@
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "en_GB.UTF-8/UTF-8"
+    ];
     extraLocaleSettings = {
       LC_ALL = "en_US.UTF-8";
     };
